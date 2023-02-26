189825abf8fa0750cd456ae0bf5d6cfda6f27d19
Heidingg <tnguyen480@my.bcit.ca>
2023-02-26 14:42:03 -0800

    comment out for the code

--- pages/index.js
+++ pages/index.js
@@ -98,12 +98,12 @@ export default function Home() {
 setData(weatherData);
   }
   useEffect(() => {
-    if (grabWeather.current === true) {
+    //if (grabWeather.current === true) {
       fetchWeather();
-    }
-    return () => {
-      grabWeather.current = true;
-    }
+    //}
+    //return () => {
+      //grabWeather.current = true;
+    //}
   }, []);
     const current = new Date();
     const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
